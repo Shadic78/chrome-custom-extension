e1439a371d35fbcb676864a1e068bb5a89478134
Shadic78 <carlosjosue7889@gmail.com>
2022-05-22 13:29:54 -0500

    Created extension

---
+++ popup.js
@@ -0,0 +1,22 @@
+const skipBtn = document.getElementById('skipBtn');
+
+const navigateToDownload = () => {
+  const getEncodedUrl = () => {
+    return location.href.match('\/#([A-Za-z0-9+/=]+)')[1]
+  };
+  
+  const getDecodedUrl = (url) => {
+    return atob(url);
+  };
+
+  const encodedUrl = getEncodedUrl();
+  window.open(getDecodedUrl(encodedUrl));
+};
+
+skipBtn.addEventListener('click', async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: navigateToDownload,
+  });
+});
