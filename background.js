229da92d6c93e74cfc124683f0aef13b1f48d725
Shadic78 <carlosjosue7889@gmail.com>
2022-05-22 18:16:44 -0500

    Added auto redirect to skip ads

--- background.js
+++ background.js
@@ -1,3 +1,20 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('Extension installed');
-});
+const navigateToDownload = () => {
+  const getEncodedUrl = () => {
+    return location.href.match('\/#([A-Za-z0-9+/=]+)')[1]
+  };
+  
+  const getDecodedUrl = (url) => {
+    return atob(url);
+  };
+
+  const encodedUrl = getEncodedUrl();
+  location.href = getDecodedUrl(encodedUrl);
+};
+
+chrome.webNavigation.onBeforeNavigate.addListener(async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: navigateToDownload,
+  });
+}, {url: [{hostSuffix: 'compul.us'}, {hostSuffix: 'compul.in'}]});
