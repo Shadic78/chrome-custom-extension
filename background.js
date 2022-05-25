a9f41f65ecf001e4841218d024f69ea4af45396f
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-05-24 21:50:26 -0500

    added prettier config & reformatted code

--- background.js
+++ background.js
@@ -6,24 +6,47 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ options: defaultConfig });
 });
 
-chrome.webNavigation.onBeforeNavigate.addListener(async () => {
-  chrome.storage.sync.get('options', async ({options}) => {
-    if(!options.ctAutoRedirect) return;
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      files: ['/scripts/ctv-bypass.js'],
+chrome.webNavigation.onBeforeNavigate.addListener(
+  async () => {
+    chrome.storage.sync.get('options', async ({ options }) => {
+      if (!options.ctAutoRedirect) return;
+      let [tab] = await chrome.tabs.query({
+        active: true,
+        currentWindow: true,
+      });
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ['/scripts/ctv-bypass.js'],
+      });
     });
-  });
-}, {url: [{hostSuffix: 'compul.us'}, {hostSuffix: 'compul.in'}, {hostSuffix: 'ctvout.buzz'}]});
+  },
+  {
+    url: [
+      { hostSuffix: 'compul.us' },
+      { hostSuffix: 'compul.in' },
+      { hostSuffix: 'ctvout.buzz' },
+    ],
+  }
+);
 
-chrome.webNavigation.onBeforeNavigate.addListener(async () => {
-  chrome.storage.sync.get('options', async ({options}) => {
-    if(!options.jpShowLinks) return;
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      files: ['/scripts/jp-bypass.js'],
+chrome.webNavigation.onBeforeNavigate.addListener(
+  async () => {
+    chrome.storage.sync.get('options', async ({ options }) => {
+      if (!options.jpShowLinks) return;
+      let [tab] = await chrome.tabs.query({
+        active: true,
+        currentWindow: true,
+      });
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ['/scripts/jp-bypass.js'],
+      });
     });
-  });
-}, {url: [{hostSuffix: 'biblioteca.japan-paw.net'}, {hostSuffix: 'biblioteca.japan-paw.wtf'}]});
+  },
+  {
+    url: [
+      { hostSuffix: 'biblioteca.japan-paw.net' },
+      { hostSuffix: 'biblioteca.japan-paw.wtf' },
+    ],
+  }
+);
--- scripts/ctv-bypass.js
+++ scripts/ctv-bypass.js
@@ -1,5 +1,5 @@
 const getEncodedUrl = () => {
-  return location.href.match('\/#([A-Za-z0-9+/=]+)')[1];
+  return location.href.match('/#([A-Za-z0-9+/=]+)')[1];
 };
 
 const getDecodedUrl = (url) => {
